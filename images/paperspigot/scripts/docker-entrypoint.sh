e03925f21d7fe53033a0d25a5ee7d42c6f1f362a
Eric Zhao <21zhaoe@protonmail.com>
2021-03-19 12:56:07 -0400

    paperspigot: Update configs

--- images/paperspigot/scripts/docker-entrypoint.sh
+++ images/paperspigot/scripts/docker-entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" = 'serve' ]];  then
+if [[ "$1" = 'serve' ]]; then
 
-  # Start server
-  exec java -jar $JAVA_ARGS \
-    -Xmx$JAVA_HEAP_SIZE -Xms$JAVA_HEAP_SIZE \
-    $SERVER_PATH/paper.jar \
-    $SPIGOT_ARGS \
-    --bukkit-settings $CONFIG_PATH/bukkit.yml --plugins $PLUGINS_PATH --world-dir $WORLDS_PATH --spigot-settings $CONFIG_PATH/spigot.yml --commands-settings $CONFIG_PATH/commands.yml --config $PROPERTIES_LOCATION \
-    --paper-settings $CONFIG_PATH/paper.yml \
-    $PAPERSPIGOT_ARGS
+	# Start server
+	exec java -jar $JAVA_ARGS \
+		-Xmx$JAVA_HEAP_SIZE -Xms$JAVA_HEAP_SIZE \
+		$SERVER_PATH/paper.jar \
+		$SPIGOT_ARGS \
+		--bukkit-settings $CONFIG_PATH/bukkit.yml \
+		--spigot-settings $CONFIG_PATH/spigot.yml \
+		--commands-settings $CONFIG_PATH/commands.yml \
+		--paper-settings $CONFIG_PATH/paper.yml \
+		--config $PROPERTIES_LOCATION \
+		--plugins $PLUGINS_PATH \
+		--world-dir $WORLDS_PATH \
+		$PAPERSPIGOT_ARGS
 fi
 
 exec "$@"
