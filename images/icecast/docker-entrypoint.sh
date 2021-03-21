3bf1ab33a05569099fc5b84f8493a1086b5d8192
Eric Zhao <21zhaoe@protonmail.com>
2021-03-21 11:45:15 -0400

    mpd: Use .env for secrets

--- images/icecast/docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -i "s/{{PASSWORD}}/$(cat ${PASSWORD_FILE})/" /usr/share/icecast/icecast.xml
-icecast -c /usr/share/icecast/icecast.xml
--- images/mpd/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sed "s/{{PASSWORD}}/$(cat ${PASSWORD_FILE})/" /config/mpd.conf | 
-  sed "s~{{LIBRARY}}~${LIBRARY}~" | 
-  sed "s~{{PLAYLISTS}}~${PLAYLISTS}~" >/etc/mpd.conf
-
-mpd --verbose --stdout --no-daemon /etc/mpd.conf
